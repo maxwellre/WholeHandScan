b0b7609b2ecf5344d5142e4e0ad41cf8989a91f7
maxwellre <yitianshao@gmail.com>
2019-01-30 18:23:03 -0800

    Producing UH response patterns.

--- DataVisualization/UltrahapticsAllFreqBandVisualization.m
+++ DataVisualization/UltrahapticsAllFreqBandVisualization.m
@@ -1,19 +1,21 @@
 %% Visualize PDV data 
-% Created on 01/14/2019 based on 'FrequencyBPNoiseCompare.m'
+% Created on 01/29/2019 based on 'UltrahapticsVisualization.m'
 % -------------------------------------------------------------------------
 % clear all
 % -------------------------------------------------------------------------
-dataName = 'Greg_MovingSpot_1ms_Dir1_1';
-% dataName = 'Greg_MovingSpot_11ms_Dir1_1';
+% dataName = 'Greg_MovingSpot_1ms_Dir1';
+dataName = 'Greg_MovingSpot_11ms_Dir1';
 % -------------------------------------------------------------------------
 MapPath = './UltrahapticsMeasurement/';
-DataPath = sprintf('../Data_Ultrahaptics/%s.svd',dataName);
+DataPath = sprintf('../Data_Ultrahaptics/%s_1.svd',dataName);
+OutputPath = './UltrahapticsVideo/';
 
-cleanDataPath = sprintf('../Data_Ultrahaptics/Old_mat/%s.mat',dataName(1:end-2));
-load(cleanDataPath);
+cleanDataPath1 = sprintf('../Data_Ultrahaptics/%s_1.mat',dataName);
+load(cleanDataPath1);
+% cleanDataPath2 = sprintf('../Data_Ultrahaptics/%s_2.mat',dataName);
+% load(cleanDataPath2);
 
 Alpha = 25.5; % (mm)
-% Alpha = 32;
 C = 0.4;
 px2mm = 0.289;  % (mm/pixel)
 interp_radius = 100;
@@ -43,10 +45,8 @@ for i = 1:locator_num
     end
 end
 
-% maskImg = imread([MapPath,sprintf('%s_Mask.jpg',dataName)]);
 maskImg = imread([MapPath,'Greg_MovingSpot_1ms_Dir1_1_Mask.jpg']);
 maskImg = rgb2gray(maskImg);
-% pointImg = imread([MapPath,sprintf('%s_MP.jpg',dataName)]);
 pointImg = imread([MapPath,'Greg_MovingSpot_1ms_Dir1_1_MP.jpg']);
 pointImg = rgb2gray(pointImg); 
 
@@ -61,66 +61,58 @@ MP_Score = cumsum([0; xDiff]).*10000 + MP_Posi(:,1);
 [~,ind] = sort(MP_Score);
 MP_Posi = MP_Posi(ind,:);
 
-% figure('Position',[60,60,1840,880],'Color','w');
-% imshow(imread([MapPath,'Greg_MovingSpot_1ms_Dir1_1_MP.jpg']))
-% for i = 1:locator_num
-%     text(MP_Posi(i,2),MP_Posi(i,1),num2str(i),'Color','r')
-% end
-
 %%
-allXYPosi = allPositions;
-remain_ind = zeros(locator_num,1);
-for i = 1:locator_num
-    posi_diff = bsxfun(@minus,positions,allXYPosi(i,:)); 
-    remain_ind(i) = (sum(posi_diff(:) == 0) == 3);
+% slow_factor = 25;
+slow_factor = 125;
+% slow_factor = 250;
+
+% Sequence 20,40,80,120,160,200,240,280,320,360,400,440,480,520,560,600,640 Hz
+freqBand = [20,40,80,160,240,320,400,480,560,640];
+freqBandNum = length(freqBand)-1;
+
+% truncData = 0.5*y_vib_clean1 + 0.5*y_vib_clean2;
+
+dataEn = rms(y_vib_sync');
+sorted_En = sort(dataEn);
+threshold = sorted_En(ceil(0.92*length(sorted_En)));
+% threshold = sorted_En(ceil(0.88*length(sorted_En)));
+remain_ind = (dataEn < threshold);
+
+temp_fig = figure('Position',[60,60,1840,880],'Color','w');
+imshow(imread([MapPath,'Greg_MovingSpot_1ms_Dir1_1_MP.jpg']))
+hold on;
+for i = find(remain_ind > 0)
+    text(MP_Posi(i,2),MP_Posi(i,1),num2str(i),'Color','c')
 end
-remain_ind = logical(remain_ind);
-
-%% Pre-filtering and Synchronization
-% y_filt = y;
-% 
-% start_i = 900;
-% truncLen = 27400 - start_i;
-% 
-% truncData = NaN(locator_num,truncLen);
-% truncData(end,:) = y_filt(end,start_i+(1:truncLen));
-% % plot(truncData(end,:)); hold on;
-% for i = 1:locator_num-1
-%     [acor,lags] = xcorr(ref(end,:),ref(i,:));
-%     [~,max_i] = max(abs(acor));
-%     lagDiff = lags(max_i);
-%     truncData(i,:) = y_filt(i,start_i-lagDiff+(1:truncLen));
-% %     plot(truncData(i,:));
-% end
-
-%%
-truncData = 0.5*y_vib_clean1 + 0.5*y_vib_clean2;
+for i = find(remain_ind == 0)
+    scatter(MP_Posi(i,2),MP_Posi(i,1),18,'r','filled');
+end
+title(sprintf('%d Point Removed',sum(remain_ind == 0)));
+hold off;
+close(temp_fig);
 
 avgWinLen = 20;
+hammWin = repmat(hamming(2*avgWinLen),[1,sum(remain_ind)]);
+
+for f_i = 1:freqBandNum
+filteredData = bandpass(y_vib_sync(remain_ind,:)',...
+    [freqBand(f_i),freqBand(f_i+1)],Fs);
 
 y_avg = [];
-% for i = 0:88
-for i = 0:450
-% for i = 0:348
-% for i = 0:1750
+for i = 0:100
+% for i = 0:1310
     slct_ind = i*avgWinLen+(1:(2*avgWinLen));
-    y_avg = [y_avg;mean(abs(truncData(:,slct_ind)),2)'];
+    y_avg = [y_avg;rms(hammWin.*filteredData(slct_ind,:))];
 end
-% USE WINDOW FUNCTION
-% CONSIDER RMS?
 
-% slow_factor = 25;
-% slow_factor = 125;
-slow_factor = 250;
 frame_rate = Fs/avgWinLen/slow_factor;
 
-v_h = VideoWriter(sprintf('%s_slow%dx_%.ffps.avi',dataName,slow_factor,...
-    frame_rate));
+v_h = VideoWriter(sprintf('%s%s_slow%dx_%.ffps_%d-%dHz.avi',OutputPath,...
+    dataName,slow_factor,frame_rate,freqBand(f_i),freqBand(f_i+1)));
 v_h.FrameRate = frame_rate;
 open(v_h);
 
-colorRange = [min(y_avg(:)),max(y_avg(:))*0.8*17/Alpha];
-% colorRange = [min(y_avg(:)),max(y_avg(:))*17/Alpha];
+colorRange = [min(y_avg(:)),max(y_avg(:))*0.7*17/Alpha];
 
 curr_fig = figure('Position',[60,60,1840,880],'Color','w');
 colormap(jet(1000));
@@ -132,8 +124,12 @@ surf(flipud(interpImg),'EdgeColor','none');
 caxis(colorRange);
 view(2)
 axis equal; axis off;
-title(sprintf('Time = %.0f ms',i*avgWinLen*1000/Fs))
+title(sprintf('[%d - %d Hz] Time = %.0f ms',freqBand(f_i),freqBand(f_i+1),...
+    i*avgWinLen*1000/Fs))
 drawnow;
 writeVideo(v_h,getframe(curr_fig));
 end
 close(v_h);
+pause(0.5);
+close(curr_fig);
+end

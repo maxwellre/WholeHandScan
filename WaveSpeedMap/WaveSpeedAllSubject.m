3120f8bd98849f7905228e73e4cf39ea4a67e76a
maxwellre <yitianshao@gmail.com>
2019-02-06 03:57:27 -0800

    Cleaned the data to get better estimation of the wave speed.

--- WaveSpeedMap/WaveSpeedAllSubject.m
+++ WaveSpeedMap/WaveSpeedAllSubject.m
@@ -9,7 +9,6 @@ sbj_num = length(pathInfo);
 BPNoiseFreq = [20,40,80,120,160,200,240,280,320,360,400,440,480,520,560,...
     600,640]; 
 BPNoiseNum = length(BPNoiseFreq); % Number of BP noise in each measurement
-CC_radius = 40; % (m) Cross-correlation computation radius
 
 % Data preprocessing ------------------------------------------------------
 if ~(exist('DataInfo','var') && exist('BPNoiseY','var'))
@@ -28,12 +27,14 @@ for sbj_i = 1:sbj_num
     
     dt = mean(diff(t));
     Fs = 1/dt;
-    fprintf('Sampling Frequency = %.1f Hz/n',Fs);
+    fprintf('Sampling Frequency = %.1f Hz\n',Fs);
     
     % Getting Measurement Point (MP) coordinates
     DataInfo{sbj_i,3} = GetXYZCoordinates(dataName, 0);
     
     locator_num = size(y,1);
+    
+    y_filt = bandpass(y',[10 800],Fs)';
 
     % Segment the measurement using the reference signal
     zero_ind = (abs(ref(end,:)) < 9e-4);
@@ -48,18 +49,34 @@ for sbj_i = 1:sbj_num
     % Display data segmentation result
     figure('Position',[60,60,1840,880],'Color','w');
     plot(onsite_ind);
-    title(sprintf('Subject: %s - Segment Number = %d',DataInfo{sbj_i},...
+    title(sprintf('Subject: %s - Segment Number = %d',DataInfo{sbj_i,1},...
         length(seg_ind)-1));
     hold on
     plot(ref(end,:)./max(ref(end,:)));
     plot(y(end,:)./max(abs(y(end,:))));
+    plot(y_filt(end,:)./max(abs(y(end,:))));
     plot(seg_ind,ones(size(seg_ind)),'*r')
     hold off
     drawnow;
     
     if (length(seg_ind)-1) == BPNoiseNum
-        for BPN_i = 1:BPNoiseNum
-            BPNoiseY{sbj_i,BPN_i} = y(:,seg_ind(BPN_i):seg_ind(BPN_i+1));
+        for BPN_i = 1:BPNoiseNum     
+            wb_h = waitbar(0, 'O', 'Name',sprintf('SBJ-%s %dHz',...
+            DataInfo{sbj_i,1}, BPNoiseFreq(BPN_i)));
+            for MP_i = 1:locator_num
+                waitbar(MP_i/locator_num,wb_h,...
+                    sprintf('MP %d/%d',MP_i,locator_num));
+                [r,~] = xcorr(ref(end,:),ref(MP_i,:));
+                [~,max_i] = max(r);
+                delay_i = max_i-length(ref(end,:));
+                if (delay_i ~= 0)
+                    fprintf('Subject: %s - %dHz - MP%d Sync Error\n',...
+                        DataInfo{sbj_i,1},BPNoiseFreq(BPN_i),MP_i);
+                end
+                BPNoiseY{sbj_i,BPN_i}(MP_i,:) =...
+                    y_filt(MP_i,(seg_ind(BPN_i):seg_ind(BPN_i+1))+delay_i);
+            end
+            close(wb_h);
         end
     else
         error('Data segmentation failed!')
@@ -69,7 +86,8 @@ close all;
 end
 
 %% Estimate wave speed
-MP_radius = 0.01; % (Unit: m) estimate speed from nearest neighboor within 1 cm
+MP_radius = 0.03; % (Unit: m) estimate speed from nearest neighboor within 1 cm
+Estim_radius = 0.03; % (Unit: m) 
 
 estimate_num = cell(sbj_num,BPNoiseNum);
 wavespeed = cell(sbj_num,BPNoiseNum);
@@ -86,7 +104,15 @@ for BPN_i = 1:BPNoiseNum
         
         wb_h = waitbar(0, 'O', 'Name',sprintf('SBJ-%s %dHz',...
             DataInfo{sbj_i,1}, BPNoiseFreq(BPN_i)));
-        for i = 1:MP_num
+        
+        tip_ind = find((XYZ(:,1) < (XYZ(end,1)+Estim_radius)) &...
+                (XYZ(:,1) > (XYZ(end,1)-Estim_radius)) &...
+                (XYZ(:,2) < (XYZ(end,2)+Estim_radius)) &...
+                (XYZ(:,2) > (XYZ(end,2)-Estim_radius)) &...
+                (XYZ(:,3) < (XYZ(end,3)+Estim_radius)) &...
+                (XYZ(:,3) > (XYZ(end,3)-Estim_radius)))';           
+            
+        for i = tip_ind
             waitbar(i/MP_num,wb_h,sprintf('MP %d/%d',i,MP_num));
             ind = find((XYZ(:,1) < (XYZ(i,1)+MP_radius)) &...
                 (XYZ(:,1) > (XYZ(i,1)-MP_radius)) &...
@@ -123,13 +149,15 @@ for BPN_i = 1:BPNoiseNum
 end
 
 %% Plot the Average Speed
+if 1 %---------------------------------------------------------------switch
+    
 slct_sbj = [1,2,3,4,5];
 slct_ind = 2:BPNoiseNum;
 figure('Position',[160,80,1620,600],'Color','w')
 plot(BPNoiseFreq(slct_ind),avgSpeed(slct_sbj,slct_ind)','.-','MarkerSize',20); 
 % boxplot(avgSpeed(:,slct_ind))
 box off;
-xlim([0 660])
+xlim([0 690])
 xlabel('Frequency (Hz)')
 ylabel('Avg. Speed (m/s)')
 yRange = ylim();
@@ -149,7 +177,7 @@ end
 fprintf('Order of fitting with lowest error (%.1f) = %d\n',lowestErr,...
     lowErrInd);
 
-slct_fit = 7;
+slct_fit = 6;
 f_intp = BPNoiseFreq(slct_ind(1)):BPNoiseFreq(slct_ind(end));
 fit_speed = polyval(fit_h{slct_fit},f_intp);
 
@@ -162,3 +190,82 @@ ylim(yRange);
 legend([DataInfo(slct_sbj,1);'Fitting'],'box','off');
 set(gca,'FontSize',16);
 
+end %------------------------------------------------------------switch end
+
+%% Plot the speed distribution on the whole hand
+Alpha = 25.5; % (mm)
+C = 0.4;
+px2mm = 0.289;  % (mm/pixel)
+interp_radius = 100;
+maskThreshold = 100;
+
+sbj_path = 'DigitIIBPNoise_Bha/';
+sbj_i = 4; % Subject: Bha
+
+maskImg = imread([sbj_path,'DigitIIBPNoise_Mask.jpg']);
+maskImg = rgb2gray(maskImg);
+pointImg = imread([sbj_path,'DigitIIBPNoise_MP.jpg']);
+pointImgGray = rgb2gray(pointImg); 
+
+locator_num = length(medianSpeed{sbj_i,1});
+
+% Low pass filter the chosen grid and computable area
+lpF = ones(3)/9; % 3-by-3 mean filter
+ptImg_PFilter = uint8(filter2(lpF,pointImgGray));
+MP_Posi = findMP(ptImg_PFilter, locator_num, 128);
+MP_Posi = round(MP_Posi); % Round the position to get index
+xDiff = diff(MP_Posi(:,2));
+xDiff(abs(xDiff)<5) = 0;
+MP_Score = cumsum([0; xDiff]).*10000 + MP_Posi(:,1);
+[~,ind] = sort(MP_Score);
+MP_Posi = MP_Posi(ind,:);
+
+handRangeX = [min(MP_Posi(:,2))-20,max(MP_Posi(:,2))+20];
+handRangeY = [min(MP_Posi(:,1))-20,max(MP_Posi(:,1))+20];
+
+% % % Validate MP position estimation
+% imshow(pointImg);
+% for i = 1:locator_num
+%     text(MP_Posi(i,2),MP_Posi(i,1),num2str(i),'Color','r')
+% end
+
+sbp_w = 0.24; % Subplot width (0 - 1)
+sbp_h = 0.24; % Subplot height (0 - 1)
+
+slct_ind = 2:BPNoiseNum;
+figure('Position',[160,80,1620,860],'Color','w');
+colormap(jet(1000))
+for i = 1:length(slct_ind)
+    r_n = floor((i-1)/4)+1;
+    c_n = mod(i-1,4);
+    subplot('Position',[0.005+c_n*sbp_w,0.98-r_n*sbp_h,sbp_w,sbp_h-0.01]);
+    sbj_avgSpeed = medianSpeed{sbj_i,slct_ind(i)};
+    
+    discard_ind = (sbj_avgSpeed > 80);
+    sbj_avgSpeed(discard_ind) = NaN;
+    
+    imshow([sbj_path,'DigitIIBPNoise_Contour.jpg']);
+    xlim(handRangeX); ylim(handRangeY)
+    hold on
+    scatter(MP_Posi(:,2),MP_Posi(:,1),30,sbj_avgSpeed,'filled');
+    hold off
+%     caxis(yRange)
+%     caxis([0 50])
+    c_h = colorbar('Ticks',[]);
+    c_h.Label.String = sprintf('%.0f - %.0f (m/s)',caxis);
+
+%     
+%     interpImg = interpMP(maskImg, MP_Posi,sbj_avgSpeed',maskThreshold,...
+%         interp_radius, Alpha, C, px2mm);      
+%     sc_h = imagesc(interpImg); 
+%     set(sc_h,'AlphaData',~isnan(interpImg));
+%     view(2);
+%     xlim(handRangeX); ylim(handRangeY)
+%     axis equal; axis off;
+%     title(sprintf('%d Hz',BPNoiseFreq(slct_ind(i))));
+%     c_h = colorbar('Ticks',[]);
+%     c_h.Label.String = sprintf('%.0f - %.0f (m/s)',caxis);
+
+    title(sprintf('%d Hz %.0f - %.0f (m/s)',BPNoiseFreq(slct_ind(i)),caxis));
+    drawnow;
+end
